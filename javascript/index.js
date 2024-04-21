4d250d717962842e9ffc4032dba83883ace8ee2f
LexyVng <163355297+LexyVng@users.noreply.github.com>
2024-04-21 14:21:40 +0100

    Updated AI prompt

--- javascript/index.js
+++ javascript/index.js
@@ -21,7 +21,7 @@ function getJoke() {
   let context =
     "You are a funny AI Assistant who likes telling jokes. Please provide a different joke each time. Please provide the joke in HTML format. Example: <p>this is your answer</p>. The question and the answer must be in different paragraphs.";
   let prompt =
-    "Please tell a short and unique joke about anything in the world. Please provide a different joke each time.";
+    "Please tell a short and unique joke about anything in the world. Please provide a different joke each time you are asked. Please include emojis at the end which are relevant to the joke.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayJoke);
