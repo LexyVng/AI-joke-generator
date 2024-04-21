22ec0adc4ba410fdbd1e7e81b726bb000aa37c18
LexyVng <163355297+LexyVng@users.noreply.github.com>
2024-04-21 13:59:35 +0100

    Created the function which calls the AI API
    Used prompt engineering techniques to ensure the AI returns appropriate results.

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,13 @@
+function getJoke() {
+  let apiKey = "2b4a0533t1055afa3fbo41efac5059ad";
+  let context =
+    "You are a funny AI Assistant who likes telling jokes. Please provide a different joke each time. Please provide the joke in HTML format. Example: <p>this is your answer</p>. The question and the answer must be in different paragraphs.";
+  let prompt =
+    "Please tell a short and unique joke about anything in the world. Please provide a different joke each time.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayJoke);
+}
+
 let buttonElement = document.querySelector("#button");
 buttonElement.addEventListener("click", getJoke);
